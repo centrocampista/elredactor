9a96f207270d34ec561bf64b211c0eb4894c5ea5
Darek Luczynski <rayzku@gmail.com>
2026-07-08 21:57:43 +0200

    feat; adding qdrant lifespan setup

--- app/config.py
+++ app/config.py
@@ -4,15 +4,34 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    
     postgres_user: str
     postgres_password: str
     postgres_db: str
     postgres_port: int = 5432
-    postgres_host: str = "postgres"
-
+    postgres_host: str
+    
     environment: Literal["development", "staging", "production"] = "production"
     debug: bool = False
 
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgressql+asyncpg://"
+            f"{self.postgres_user}:{self.postgres_password}"
+            f"@{self.postgres_host}:{self.postgres_port}/"
+            f"{self.postgres_db}"
+        )
+        
+    groq_api_key: str
+
+    qdrant_service_host: str
+    qdrant_service_http_port: str
+    qdrant_service_grpc_port: str
+    qdrant_service_read_only_api_key: str
+    qdrant_service_api_key: str
+    qdrant_service_prefer_grpc: bool = False
+    
     model_config = SettingsConfigDict(
         env_file=".env", extra="ignore", env_file_encoding="utf-8", case_sensitive=False
     )
---
+++ app/vector_db/lifespan.py
@@ -0,0 +1,18 @@
+
+from contextlib import asynccontextmanager
+from .session import qdrant_reader, qdrant_writer
+
+@asynccontextmanager
+async def qdrant_lifespan():
+    if not await check_qdrant(): 
+        raise RuntimeError("Couldn't connect to Qdrant vector database")
+    yield
+    await qdrant_reader.close()
+    await qdrant_writer.close()
+
+async def check_qdrant() -> bool:                                                                                                                                                      
+    try:                                                                                                                                                                             
+        await qdrant_reader.get_collections()                                                                                                                                          
+        return True                                                                                                                                                                    
+    except Exception:
+        return False
