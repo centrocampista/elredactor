d968253933794aaba8df835c9222a197dccb9bc6
Darek Luczynski <rayzku@gmail.com>
2026-07-17 18:01:48 +0200

    feat; adopting unit tests

--- tests/unit/test_users.py
+++ tests/unit/test_users.py
@@ -23,19 +23,15 @@ def test_user_id_is_primey_key():
     col = User.__table__.columns["id"]
     assert col.primary_key is True
 
-
-# first_name="Jan", last_name="Kowalski", email="jan@example.com"
-
-
 @pytest.mark.unit
 @pytest.mark.parametrize(
     "user_create",
-    (UserCreate(first_name="Jan", last_name="Kowalski", email="jan@example.com")),
+    [UserCreate(first_name="Jan", last_name="Kowalski", email="jan@example.com"),]
 )
 async def test_create_user(mock_session, user_create):
-    user = await create_user(mock_session)
+    user = await create_user(mock_session, user_create)
     mock_session.add.assert_called_once()
-    mock_session.commit.assert_called_once()
+    mock_session.flush.assert_called_once()
     assert user.email == user_create.email
 
 
