4f215fef08f02fe05f75eb61e557dad5c1ff2669
Darek Luczynski <rayzku@gmail.com>
2026-07-17 10:02:15 +0200

    feat; crud create user

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,4 +1,12 @@
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user import User
+from app.schemas.user import UserCreate
 
-async def create_user(db_session, first_name: str, last_name: str, email: str):
-    ...    
+
+async def create_user(db_session: AsyncSession, data: UserCreate) -> User:
+    user = User(**data.model_dump)
+    await db_session.add(user)
+    await db_session.flush()
+    await db_session.refresh(user)
+    return user
