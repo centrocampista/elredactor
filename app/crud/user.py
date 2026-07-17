ba2d8e0cc1ca6f2bbd3c0116cef43ef888a2a8b7
Darek Luczynski <rayzku@gmail.com>
2026-07-17 15:52:11 +0200

    feat; crud create user and update user and pydantic schemas for update,create and response

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,12 +1,21 @@
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.user import User
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserUpdate
 
 
 async def create_user(db_session: AsyncSession, data: UserCreate) -> User:
     user = User(**data.model_dump)
     await db_session.add(user)
+    await db_session.flush()
+    await db_session.refresh(user)
+    return user
+
+async def update_user(db_session: AsyncSession, user: User, data: UserUpdate) -> User:
+    changes = data.model_dump(exclude_unset=True)
+    changes.pop('email')
+    for k, v in changes.items():
+        setattr(user, k, v)
     await db_session.flush()
     await db_session.refresh(user)
     return user
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,8 +1,25 @@
-from pydantic import BaseModel, EmailStr
+import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 class UserCreate(BaseModel):
     first_name: str
     last_name: str
     email: EmailStr
     
-  
+
+class UserUpdate(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    email: EmailStr
+    
+
+class UserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: UUID
+    first_name: str
+    last_name: str
+    email: str
+    created_at: datetime
+    updated_at: datetime
